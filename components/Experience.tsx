2df952da08e53101a16077433a48a528e36ee74c
Sourav Biswas <souravbiswas19april@gmail.com>
2024-12-18 23:12:45 +0530

    Work Experience Added

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -14,7 +14,7 @@ const Experience = () => {
             {workExperience.map((card)=>(
                 <Button
                 key={card.id}
-                duration={Math.floor(Math.random()*1000)+1000}
+                duration={Math.floor(Math.random()*10000)+10000}
                 borderRadius='1.75rem'
                 className='flex-1 text-white border-neutral-200 dark:border-slate-800'>
                     <div className='flex lg:flex-row flex-col lg:items-center p-3 py-6 md:p-5 lg:p-10 gap-2'>
